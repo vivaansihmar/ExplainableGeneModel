1bcadd520887e2db015fbc518b62dfaca9984259
vivaansihmar <vivaansihmar@gmail.com>
2025-10-24 16:39:35 +0530

    atten model

--- src/Dl.py
+++ src/Dl.py
@@ -6,12 +6,29 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
-import shap
+from sklearn.metrics import accuracy_score, f1_score, classification_report, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
 import tensorflow as tf
+from tensorflow.keras.layers import Input, Dense, Dropout, Layer, Multiply
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow import keras
-from tensorflow.keras import layers
 import os, random
+import time
 
+start_timme = time.time()
 
+DATA_PATH = "Data/METABRIC_RNA_Mutation.csv"
+MODEL_OUT = "output/models/attention_model.keras"
+PLOT_OUT = "output/plots/training_curves.png"
+ATTN_OUT = "output/tables/attention_weights.csv"
+
+data = pd.read_csv(DATA_PATH)
+print("Data loaded:", data.shape)
+
+data = data.dropna()
+TARGET_COL = 'pam50_+_claudin-low_subtype'
+
+X =data.dropna(columns=[TARGET_COL])
+y = data[TARGET_COL]
